89650e5a9933d32bad4c5bd0c16d4a8aff87d943
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2020-10-30 01:36:15 +0800

    Create Tester.java

---
+++ com/ntustars/controller/Tester.java
@@ -0,0 +1,21 @@
+package sce.cz2002.assignment;
+
+public class Tester {   
+
+    public static void main(String[] args) throws IOException{     
+    	
+        AdminMgr admin = new AdminMgr();
+        //admin.updStudParticulars();
+        //admin.printAllStudent();
+        
+        admin.printStudListByIndex();
+        admin.printStudListByCourse();
+        
+        //admin.addStudInfo();
+        
+        
+    }
+
+	 
+
+}
